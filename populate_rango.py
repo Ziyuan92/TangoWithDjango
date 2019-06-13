cf59a4558601b1514ee18d661419ea45e25dfc21
Ziyuan92 <ziyuan.wang92@gmail.com>
2019-06-13 23:34:29 +1000

    Exercise changes on Models

--- populate_rango.py
+++ populate_rango.py
@@ -33,19 +33,24 @@ def populate():
     ]
 
     categories = {
-        'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages}
+        'Python': {
+            'pages': python_pages, 'views': 128, 'likes': 64},
+        'Django': {
+            'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Frameworks': {
+            'pages': other_pages, 'views': 32, 'likes':16}
     }
 
     for cat, cat_data in categories.items():
         print('cat is: ' + cat)
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes']) 
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
